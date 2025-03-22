e94ac70197d772e5e64aec9b7646297056a5e4f6
verb0t <mykola.i.balii@gmail.com>
2025-03-22 03:44:23 +0200

    minor fucking fixes

--- server/api/books/index.get.js
+++ server/api/books/index.get.js
@@ -2,17 +2,22 @@ import { Book } from "~/server/database/models/Book.js";
 export default defineEventHandler(async (event) => {
   const query = getQuery(event);
   try {
-    // == should be there :)
+    const whereList = {};
+
+    if (query.is_on_sale) {
+      whereList.is_on_sale = query.is_on_sale == "true";
+    }
+
+    if (query.draft) {
+      whereList.draft = query.draft == "true";
+    }
+    // == should be there 🙂
     const books = await Book.findAll({
-      where: {
-        draft: query.draft == "true",
-        is_on_sale: query.is_on_sale == "true",
-      },
+      where: whereList,
     });
     setResponseStatus(event, 200);
     return books;
   } catch (err) {
-    setResponseStatus(event, 500);
     const error = createError({
       message: "Something went wrong",
       statusCode: 500,
