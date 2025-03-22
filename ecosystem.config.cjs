c58a8b0d431706486cefb9194fd7508ceda55749
verb0t <mykola.i.balii@gmail.com>
2025-03-22 23:30:29 +0200

    manual load models

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: "Obert_Book_Store",
+      name: "obert",
       exec_mode: "cluster",
       instances: 1,
       script: "./.output/server/index.mjs",
-      env: {
-        PORT: 3000,
-        NODE_ENV: "production",
-      },
     },
   ],
 };
+
+// export $(cat .env | xargs) && pm2 start .output/server/index.mjs --name obert
+// Ubuntu
--- server/plugins/db.js
+++ server/plugins/db.js
@@ -1,11 +1,23 @@
 import consola from "consola";
+import { Purchase } from "../database/models/Purchase";
+import { Customer } from "../database/models/Customer";
 export default defineNitroPlugin(async (_nitro) => {
   try {
     consola.success("Connecting to database...");
     await sequelize.authenticate();
     consola.success("Connection has been established successfully.");
 
-    const models = sequelize.models;
+    const PurchaseM = Purchase(sequelize, Sequelize.DataTypes);
+    const CustomerM = Customer(sequelize, Sequelize.DataTypes);
+
+    const models = { Purchase: PurchaseM, Customer: CustomerM };
+
+    // Now associate
+    Object.values(models).forEach((model) => {
+      if (typeof model.associate === "function") {
+        model.associate(models);
+      }
+    });
     console.log("Available models:", Object.keys(models));
 
     Object.values(models).forEach((model) => {
