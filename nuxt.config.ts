c1d170b02bd0cb08f82625beae6de03728dc8e8b
Pasha <kramarpavlo@gmail.com>
2025-04-03 21:10:53 +0300

    fix: config v10

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,58 +21,33 @@ export default defineNuxtConfig({
   security: {
     headers: {
       contentSecurityPolicy: {
-        "frame-src": ["'self'", "https://secure.wayforpay.com", "https://widget.wayforpay.com"],
-        "frame-ancestors": ["'self'", "https://secure.wayforpay.com"],
-        "form-action": ["'self'", "https://secure.wayforpay.com", "https://widget.wayforpay.com"],
+        "form-action": ["'self'", "https://secure.wayforpay.com"],
+        "frame-src": ["'self'", "https://secure.wayforpay.com"],
         "script-src": [
           "'self'",
           "'unsafe-inline'",
           "https://secure.wayforpay.com",
-          "https://widget.wayforpay.com"
         ],
-        "connect-src": ["'self'",
-           "https://secure.wayforpay.com",
-           "https://api.wayforpay.com",
-          "https://api.novaposhta.ua"]
       },
-      referrerPolicy: "strict-origin-when-cross-origin",
-      crossOriginResourcePolicy: "cross-origin",
-      xFrameOptions: false 
-    }
+    },
   },
 
-  nitro: {
-    routeRules: {
-      '/api/wayforpay/**': {
-        cors: true,
-        headers: {
-          'Access-Control-Allow-Origin': 'https://obert.com.ua',
-          'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-          'Access-Control-Allow-Credentials': 'true',
-          'Access-Control-Expose-Headers': '*'
-        }
-      
-      },
-      '/api/**': {
-        cors: true, 
-        headers: {
-          'Access-Control-Allow-Origin': 'https://obert.com.ua',
-          'Access-Control-Allow-Credentials': 'true'
+  routeRules: {
+    "/api/**": {
+      security: {
+        corsHandler: {
+          origin: "*",
+          methods: "*",
+          credentials: true,
         },
-      }
-    }
+      },
+    },
   },
 
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-
   runtimeConfig: {
-    public: {
-      siteUrl: 'https://obert.com.ua',
-      wayforpayUrl: 'https://secure.wayforpay.com'
-    },
     jwtSecret: process.env.JWT_SECRET,
     wayforpayMerchantLogin: process.env.WAYFORPAY_MERCHANT_LOGIN,
     wayforpaySecretKey: process.env.WAYFORPAY_SECRET_KEY,
